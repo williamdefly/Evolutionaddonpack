48ff5c27a9e4837f08cbf2012ff7f8711f8ef368
Williamdefly <williammarquis@hotmail.fr>
2017-01-22 20:31:31 +0100

    5.4.0a
    add new sb_evolution gatespawner

---
+++ lua/data/gatespawner/sb_evolution.lua
@@ -0,0 +1,139 @@
+[gatespawner]
+version = 3 Group
+
+
+[stargate]
+classname=sg_tollan
+position=5934.000000 9063.000000 -29.000000
+angles=0.000 0.000 0.000
+address=90IN5A
+group=M@
+name=Celestis
+private=false
+locale=false
+[stargate]
+classname=sg_atlantis
+position=-8331.994141 8502.420898 9262.031250
+angles=0.000 -90.940 0.000
+address=VYIQ2U
+group=P@
+name=Atlantis
+private=false
+locale=false
+[stargate]
+classname=sg_sg1
+position=10240.000000 -10104.000000 9481.000000
+angles=0.000 -180.000 0.000
+address=Z4F9CJ
+group=I@
+name=Othala
+private=false
+locale=false
+[stargate]
+classname=sg_atlantis
+position=-760.700867 -3688.382812 12332.214844
+angles=-0.000 -5.330 0.000
+address=SIN4KU
+group=P@
+name=Porte spacial wraith
+private=false
+locale=false
+[stargate]
+classname=sg_sg1
+position=-8543.154297 -7152.326660 -5267.102539
+angles=-0.004 -92.176 0.003
+address=4R6OX8
+group=M@
+name=Abydos
+private=false
+locale=false
+[stargate]
+classname=sg_atlantis
+position=12601.319336 -568.514893 -7658.819336
+angles=0.000 -89.181 0.000
+address=243LQD
+group=P@
+name=Asuras
+private=false
+locale=false
+[stargate]
+classname=sg_universe
+position=-14029.328125 -9863.873047 -15813.968750
+angles=0.000 -92.200 0.000
+address=
+group=SGI
+name=Spawn
+private=false
+locale=false
+blocked=false
+[iris]
+classname=sg_iris
+position=-8332.000977 8502.020508 9262.031250
+angles=0.000 -90.940 0.000
+model=models/zup/stargate/sga_shield.mdl
+[dhd]
+classname=dhd_milk
+position=6213.940430 9296.312500 -161.632385
+angles=15.000 2.100 0.000
+destroyed=false
+[dhd]
+classname=dhd_atl_city
+position=-8630.553711 7807.488281 9316.346680
+angles=-0.035 -152.639 0.039
+[dhd]
+classname=dhd_milk
+position=-8547.618164 -7355.498535 -5395.857910
+angles=15.000 -87.640 0.000
+destroyed=false
+[dhd]
+classname=dhd_atl
+position=12779.102539 -648.740479 -7763.619141
+angles=15.000 -89.621 0.000
+destroyed=false
+[dhd]
+classname=dhd_milk
+position=9908.209961 -10223.607422 9296.231445
+angles=15.000 153.560 0.000
+destroyed=false
+[sgu_stuff]
+classname=gatebearing
+position=-14029.405273 -9865.871094 -15677.968750
+angles=0.000 -92.200 0.000
+model=models/iziraider/gatebearing/bearing.mdl
+[sgu_stuff]
+classname=floor_chevron
+position=-14030.862305 -9903.843750 -15903.968750
+angles=0.000 87.800 0.000
+model=models/the_sniper_9/universe/stargate/floorchevron.mdl
+[ramp]
+classname=goauldramp
+position=-8543.147461 -7152.335938 -5413.102539
+angles=-0.004 -92.176 0.003
+[gravitycontroller]
+classname=gravity_controller
+position=-760.700806 -3688.382812 12197.214844
+angles=0.000 -95.330 180.000
+model=models/cebt/sga_pwnode.mdl
+sound=ambient/atmosphere/underground_hall_loop1.wav
+[gravitycontroller]
+classname=gravity_controller
+position=-749.738892 -3570.893066 12400.215820
+angles=60.000 84.670 -0.000
+model=models/cebt/sga_pwnode.mdl
+sound=ambient/atmosphere/underground_hall_loop1.wav
+[gravitycontroller]
+classname=gravity_controller
+position=-771.662720 -3805.872314 12400.215820
+angles=-60.000 84.670 0.000
+model=models/cebt/sga_pwnode.mdl
+sound=ambient/atmosphere/underground_hall_loop1.wav
+[cap_console]
+classname=cap_console
+position=-8829.329102 7571.471680 9332.294922
+angles=-0.000 -143.130 0.000
+model=models/markjaw/atlantis_console/console.mdl
+[cap_console]
+classname=sg_console
+position=-8954.108398 7689.834961 9332.294922
+angles=0.000 -114.310 0.000
+model=models/markjaw/atlantis_console/console.mdl
