de246c2dd007eee79823c7053f6f97f2cab5a468
Elanis <elanis@hotmail.com>
2017-05-02 19:57:35 +0200

    Close #100

---
+++ lua/data/gatespawner_group/rp_stargate_v5.lua
@@ -0,0 +1,133 @@
+[gatespawner]
+version = 3 Group
+
+
+[stargate]
+classname=sg_sg1
+position=12692.129883 -9746.379883 2542.217529
+angles=0.000 -110.000 0.000
+address=TOKRAS
+group=M@
+name=Tok'ra Homeworld
+private=false
+locale=true
+blocked=false
+sgctype=false
+[stargate]
+classname=sg_sg1
+position=10813.704102 -2112.076416 11681.031250
+angles=0.000 -90.170 0.000
+address=OTHALB
+group=I@
+name=Asgards Homeworld
+private=false
+locale=false
+blocked=false
+sgctype=false
+[stargate]
+classname=sg_sg1
+position=10376.7695 8760.6963 2561.0313
+angles=0.000 270.140 0.000
+address=ABYDOS
+group=M@
+name=Abydos world
+private=false
+locale=true
+blocked=false
+sgctype=false
+[stargate]
+classname=sg_sg1
+position=3825.810791 -5095.340332 11229.031250
+angles=0.000 0.453 -0.000
+address=ANUBIS
+group=M@
+name=Anubis Ship
+private=false
+locale=true
+blocked=false
+sgctype=false
+[stargate]
+classname=sg_sg1
+position=-5754.448730 -5508.146973 8852.031250
+angles=0.000 270.000 0.000
+address=GATEOV
+group=M@
+name=Stargate overloader world
+private=false
+locale=false
+blocked=false
+sgctype=false
+[stargate]
+classname=sg_sg1
+position=-9602.3115 10751.9893 6427.0313
+angles=0.000 90.099 -0.000
+address=TAURIS
+group=M@
+name=SGC Base
+private=false
+locale=false
+blocked=false
+sgctype=true
+[stargate]
+classname=sg_sg1
+position=-0.237238 291.300964 -13773.968750
+angles=0.000 270.000 0.000
+address=SPAWN0
+group=M@
+name=SPAWN / BUILDWORLD
+private=false
+locale=false
+blocked=false
+sgctype=false
+[stargate]
+classname=sg_tollan
+position=-9559.578125 2190.706787 9408.031250
+angles=0.000 90.000 0.000
+address=TOLANS
+group=M@
+name=Tollan Homeworld
+private=false
+locale=false
+blocked=false
+sgctype=false
+[dhd]
+classname=dhd_milk
+position=-0.236072 -138.367142 -13878.768555
+angles=15.000 -90.000 -0.000
+destroyed=false
+[dhd]
+classname=dhd_milk
+position=10929.265625 -2329.709473 11506.231445
+angles=15.000 -63.774 0.000
+destroyed=false
+[dhd]
+classname=dhd_milk
+position=4293.476074 -5091.087402 11124.231445
+angles=15.000 359.990 0.000
+destroyed=false
+[dhd]
+classname=dhd_milk
+position=10376.500000 8447.165039 2396.231201
+angles=15.000 270.256 0.000
+destroyed=false
+[dhd]
+classname=dhd_milk
+position=-9349.237305 2320.293945 9303.343750
+angles=15.000 66.312 0.000
+destroyed=false
+[dhd]
+classname=dhd_milk
+position=-5755.978516 -6140.238281 8747.231445
+angles=15.000 -90.000 -0.000
+destroyed=false
+[dhd]
+classname=dhd_milk
+position=12453.586914 -9795.916016 2425.029541
+angles=15.000 115.000 0.000
+destroyed=false
+[mobile_dhd]
+classname=mobiledhd
+position=-9629.122070 11407.301758 6355.306641
+angles=0.000 90.000 -0.000
+model=models/props_c17/computer01_keyboard.mdl
+destroyed=false
