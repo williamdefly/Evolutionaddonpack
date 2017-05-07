d4a75756d84a6c110fc9cca69dea6a705b65c3b7
Elanis <elanis@hotmail.com>
2017-05-07 12:16:16 +0200

    Fixed a bug in un_stable effect

--- lua/eap_librairies/shared/general.lua
+++ lua/eap_librairies/shared/general.lua
@@ -657,6 +657,40 @@ function Lib.LOS(ent1, enttab, postab)
 	return hitent,hitentpos
 end
 
+function Lib.ShieldTrace(pos, dir, filter)
+	local tr = Lib.Trace:New(pos, dir, filter)
+	local aim = (dir-pos):GetNormal()
+	tr.HitShield = false
+
+	if(IsValid(tr.Entity)) then 		-- special execption for when hitting avon's shield
+		local class = tr.Entity:GetClass()
+		if(class == "shield") then 	--This is a  ridiculously complex way of actually finding where the spherical shield is and not the cubic bounding box if anybody knows of a better way PLEASE tell me
+			local pos2 = tr.Entity:GetPos()
+			local rad = tr.Entity:GetNetworkedInt("size",0)
+			local relpos = tr.HitPos-pos2
+			local a = aim.x^2+aim.y^2+aim.z^2
+			local b = 2*(relpos.x*aim.x+relpos.y*aim.y+relpos.z*aim.z)
+			local c = relpos.x^2+relpos.y^2+relpos.z^2-rad^2
+			local dist = (-1*b-(b^2-4*a*c)^0.5)/(2*a)	-- Thank god for Brahmagupta
+
+			if tostring(dist) == "-1.#IND" then 	-- Sometimes the trace will hit the bounding box but end up not actually hitting the round shield and this should mean that dist is a non-real number
+				tr = Lib.ShieldTrace(tr.HitPos, dir, tr.Entity, true)
+			elseif dist < 0 then -- If the trace starts in the sphere the dist will be negative.
+				dist = (-1*b+(b^2-4*a*c)^0.5)/(2*a)
+				tr.HitPos = tr.HitPos+aim*dist
+				tr.HitNormal = (tr.HitPos-pos2):GetNormal()
+				tr.HitShield = true
+			else
+				tr.HitPos = tr.HitPos+aim*dist
+				tr.HitNormal = (tr.HitPos-pos2):GetNormal()
+				tr.HitShield = true
+			end
+		end
+	end
+
+	return tr
+end
+
 function Lib.ArePointsInsideAShield(points) -- Thanks PyroSpirit for the help :})<<<(P.S. It has a moustache).
 	local IsInShield = {}
 	local num = table.getn(points)
