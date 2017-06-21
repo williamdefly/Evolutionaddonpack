712c28961931e5185d5b8b627b80dc78b44a1e3e
Elanis <elanis@hotmail.com>
2017-06-21 19:39:38 +0200

    Close #114

---
+++ lua/data/gatespawner_group/gm_sgc_to_atlantis.lua
@@ -0,0 +1,115 @@
+[gatespawner]
+version = 3 Group
+
+
+[stargate]
+classname=sg_sg1
+position=-429.943970 -3989.267334 1011.531250
+angles=0.000 -0.164 0.000
+address=P2X3YZ
+group=M@
+name=Earth (SGC)
+private=true
+locale=true
+[stargate]
+classname=sg_sg1
+position=581.192993 -10031.438477 3337.031250
+angles=0.000 180.158 0.000
+address=P6R125
+group=M@
+name=Midway (Pegasus)
+private=true
+locale=true
+[stargate]
+classname=sg_atlantis
+position=-922.379883 -10029.551758 3337.031250
+angles=0.000 0.662 -0.000
+address=M6R125
+group=P@
+name=Midway (Pegasus)
+private=true
+locale=true
+[stargate]
+classname=sg_atlantis
+position=39.523247 -4470.091797 7250.531250
+angles=0.000 -179.200 0.000
+address=M35217
+group=P@
+name=Lantea (ATLANTIS)
+private=false
+locale=true
+[stargate]
+classname=sg_atlantis
+position=1740.948242 -1913.565918 -3037.336182
+angles=0.000 201.526 0.000
+address=M4H210
+group=P@
+name=Genii Homeworld
+private=false
+locale=true
+[stargate]
+classname=sg_atlantis
+position=15748.620117 -13805.246094 -15772.658203
+angles=-0.000 137.831 0.000
+address=SKYBOX
+group=P@
+name=Skybox Gate
+private=false
+locale=true
+[dhd]
+classname=dhd_atl_city
+position=466.484680 -9749.186523 3368.367920
+angles=0.027 89.984 0.038
+[dhd]
+classname=dhd_atl_city
+position=-538.120667 -4078.556152 7304.970215
+angles=0.000 134.448 0.000
+[dhd]
+classname=dhd_atl
+position=1633.712402 -2149.405029 -3192.032227
+angles=15.000 143.511 0.000
+destroyed=false
+[mobile_dhd]
+classname=mobiledhd
+position=277.395081 -3986.677734 945.806396
+angles=-0.000 -0.073 0.000
+model=models/props_c17/computer01_keyboard.mdl
+[mobile_dhd]
+classname=mobiledhd
+position=-811.517944 -9786.554688 3402.010010
+angles=0.000 90.003 -0.000
+model=models/props_c17/computer01_keyboard.mdl
+[ramp]
+classname=sgcramp
+position=-429.943970 -3989.267334 1011.531250
+angles=0.000 -0.164 0.000
+[atlantis_transporter]
+classname=atlantis_trans
+position=-339.915314 -5373.966797 7120.531250
+angles=0.000 89.831 -0.000
+name=Gate Room
+private=false
+[atlantis_transporter]
+classname=atlantis_trans
+position=-552.867798 -4947.749512 6572.531738
+angles=-0.000 90.200 0.000
+name=Canteen
+private=false
+[atlantis_transporter]
+classname=atlantis_trans
+position=-153.366104 -4274.333008 6006.531250
+angles=0.000 -0.112 0.000
+name=Lobby
+private=false
+[atlantis_transporter]
+classname=atlantis_trans
+position=168.382858 -2534.384033 5274.904297
+angles=0.068 144.674 0.003
+name=ZPM Floor
+private=false
+[atlantis_transporter]
+classname=atlantis_trans
+position=-676.365173 -2320.177979 5433.531250
+angles=0.000 -179.793 0.000
+name=Quarters
+private=false
